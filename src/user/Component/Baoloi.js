719c97ffe25b63519d675232e2b877fe6d0e36eb
HaTruongVietDuy <quanghuyvoicontre@gmail.com>
2024-05-02 11:51:26 +0700

    deploy nhanh

--- src/user/Component/Baoloi.js
+++ src/user/Component/Baoloi.js
@@ -7,6 +7,7 @@ const BaoLoi = () => {
         <h1>404 - Không tìm thấy</h1>
         <p>Sản phẩm bạn đang tìm kiếm có thể đã bị xóa, đã thay đổi tên hoặc tạm thời không khả dụng.</p>
         <a href="/" className="button">Trở lại trang chủ </a>
+        {/* <a href="/" className="button">Trở lại trang chủ </a> */}
     </div>
   );
 };
